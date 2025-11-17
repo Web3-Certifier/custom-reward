3ecb94157fafef2858351f6a500ca97b8e8128ad
spyros-zikos <zikossp@gmail.com>
2025-11-17 21:39:57 +0200

    improve interfaces

--- interfaces/IRewardFactory.sol
+++ interfaces/IRewardFactory.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.24;
 
 interface IRewardFactory {
+    enum DistributionType {CUSTOM, CONSTANT, UNIFORM}
+    enum EligibilityCriteria {NONE, CUSTOM, HOLDS_TOKEN, HOLDS_NFT}
     function createReward(
         uint256 examId,
         uint256 initialRewardAmount,
