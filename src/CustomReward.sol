299ce5f36cefdddaa63c8717a11fa958bdbe4ea1
spyros-zikos <zikossp@gmail.com>
2025-11-17 21:40:38 +0200

    update default custom reward

--- src/CustomReward.sol
+++ src/CustomReward.sol
@@ -4,12 +4,10 @@ pragma solidity ^0.8.24;
 import { ICustomReward } from "../interfaces/ICustomReward.sol";
 
 contract CustomReward is ICustomReward {
-    function getCustomRewardAmountForUser(
-        address /* user */,
-        uint256 numberOfCorrectAnswers,
-        uint256 /* s_rewardAmountPerPerson */,
-        uint256 s_rewardAmountPerCorrectAnswer
-    ) external pure returns (uint256){
-        return numberOfCorrectAnswers * s_rewardAmountPerCorrectAnswer;
+    function isEligible(address user) external view returns (bool) {
+        return true;
+    }
+    function rewardAmount(address user, uint256 distributionParameter) external view returns (uint256) {
+        return distributionParameter;
     }
 }
