f24fdd2cfa0804eb9e711076658e30a3fd852b89
spyros-zikos <zikossp@gmail.com>
2025-11-18 17:35:18 +0200

    comment out unused arguments

--- src/CustomReward.sol
+++ src/CustomReward.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.24;
 import { ICustomReward } from "../interfaces/ICustomReward.sol";
 
 contract CustomReward is ICustomReward {
-    function isEligible(address user) external view returns (bool) {
+    function isEligible(address /* user */) external view returns (bool) {
         return true;
     }
-    function rewardAmount(address user, uint256 distributionParameter) external view returns (uint256) {
+    function rewardAmount(address /* user */, uint256 distributionParameter) external view returns (uint256) {
         return distributionParameter;
     }
 }
