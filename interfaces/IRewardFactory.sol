643340a54de2b01b1c1ae5a51b6379e0601b29e7
spyros-zikos <zikossp@gmail.com>
2025-11-25 13:45:13 +0200

    update interfaces

--- interfaces/IRewardFactory.sol
+++ interfaces/IRewardFactory.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.8.24;
 
 interface IRewardFactory {
     enum DistributionType {CUSTOM, CONSTANT, UNIFORM}
-    enum EligibilityCriteria {NONE, CUSTOM, HOLDS_TOKEN, HOLDS_NFT}
+    enum EligibilityType {NONE, CUSTOM, HOLDS_TOKEN, HOLDS_NFT}
     function createReward(
         uint256 examId,
-        uint256 initialRewardAmount,
         address rewardToken,
         DistributionType distributionType,
         uint256 distributionParameter,  // has 18 decimals
-        EligibilityCriteria eligibilityCriteria,
+        EligibilityType eligibilityType,
         address eligibilityParameter
     ) external returns (address);
 }
