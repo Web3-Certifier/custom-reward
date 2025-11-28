35971f8f08c75d11da01f53aac414e9c7be1e8b0
spyros-zikos <zikossp@gmail.com>
2025-11-28 13:59:39 +0200

    fix name

--- src/CustomReward.sol
+++ src/CustomReward.sol
@@ -9,7 +9,7 @@ contract CustomReward is ICustomReward {
     }
 
     function description() external pure returns (string memory) {
-        return "Custom Reward Descriptionrjklghefgnruiter780g3457u807uiyuih47uy57u8h785uyghrhtui4ieryiueryiueryguieryguierguiertuieryhtuierytui4rytueyt9erytiuerhtuiertyeruityeruiteruitertuierbtuiertberuitberuiyntbuierytbuierytbierutyberiteruiterueruugieriyg9eryg9eryg9eryg98erug89eru98eru9g8ue98rug89eu9er8ute908utb9e8su5bhn945ynagb7p89045atpn34t";
+        return "Custom Reward Description";
     }
 
     function isEligible(address /* user */) external pure returns (bool) {
